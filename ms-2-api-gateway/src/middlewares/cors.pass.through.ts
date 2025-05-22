375e7c757e2f37b9ccf2e54fccfe9a51302875b3
Denis Volkov <to.den.volkov@gmail.com>
2025-05-22 23:05:33 +0300

    INT-123 removed PUT

--- ms-2-api-gateway/src/middlewares/cors.pass.through.ts
+++ ms-2-api-gateway/src/middlewares/cors.pass.through.ts
@@ -4,7 +4,7 @@ export default (req: IncomingMessage, res: ServerResponse, next: any): void => {
   if (req.headers.hasOwnProperty('origin') && req.headers.origin.trim() !== '') {
     const origin = req.headers.origin;
     res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Methods', [ 'GET', 'OPTIONS', 'POST', 'PUT', 'DELETE' ].join(', '));
+    res.setHeader('Access-Control-Allow-Methods', [ 'GET', 'OPTIONS', 'POST'].join(', '));
     res.setHeader('Access-Control-Expose-Headers', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, x-requested-with, content-type, accept, authorization, jwt');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
