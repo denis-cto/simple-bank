78462a04d5e706202593c34bad9d3d1003ca5555
Denis Volkov <denis.volkov@1win.pro>
2025-05-22 22:03:46 +0300

    Some dev change

--- ms-2-api-gateway/src/middlewares/cors.pass.through.ts
+++ ms-2-api-gateway/src/middlewares/cors.pass.through.ts
@@ -2,13 +2,7 @@ import { IncomingMessage, ServerResponse } from 'http';
 
 export default (req: IncomingMessage, res: ServerResponse, next: any): void => {
   if (req.headers.hasOwnProperty('origin') && req.headers.origin.trim() !== '') {
-    const origin = req.headers.origin;
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Methods', [ 'GET', 'OPTIONS', 'POST', 'PUT', 'DELETE' ].join(', '));
-    res.setHeader('Access-Control-Expose-Headers', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'origin, x-requested-with, content-type, accept, authorization, jwt');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Max-Age', 3600);
+    const origin = req.headers.origin;xw
 
     if (req.method === 'OPTIONS') {
       res.writeHead(204, {
--- ms-2-api-gateway/src/middlewares/dictionaries.ts
+++ ms-2-api-gateway/src/middlewares/dictionaries.ts
@@ -10,13 +10,6 @@ const routingCache = new Map();
 const dictionaries: Middleware = {
   // @ts-ignore
   serviceCreating: (service: Service, schema: ServiceSchema): any => {
-    if (!schema.dictionaries) {
-      return;
-    }
-    if (!schema.routes) {
-      schema.routes = [];
-    }
-
     const routes = {
       ...schema.dictionaries.options,
       aliases: {},
@@ -98,11 +91,6 @@ const baseHandler = async (callName: string, params: any, req: IRequest, res: IR
 
   if (response.statusCode !== 200) {
     return ResponseFactory.createServerErrorResponse(res, response.error);
-  } else {
-    // eslint-disable-next-line id-blacklist
-    return response.data === undefined && response.errors !== undefined
-      ? ResponseFactory.createServerErrorResponse(res, response.errors)
-      : ResponseFactory.createSuccessResponse(res, response.data);
   }
 };
 
