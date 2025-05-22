33e2ef1e4a5edab88bce0557329f562288cbd54f
Denis Volkov <to.den.volkov@gmail.com>
2025-05-22 23:12:54 +0300

    CHERRYPICKED INT-456 removed delete, updated cors again

--- ms-2-api-gateway/src/middlewares/dictionaries.ts
+++ ms-2-api-gateway/src/middlewares/dictionaries.ts
@@ -90,9 +90,6 @@ const baseHandler = async (callName: string, params: any, req: IRequest, res: IR
       }
     }
 
-    delete params.catalogName;
-    delete params.catalogMethod;
-
     response = await req.$ctx.call(`${remote}.${callName}`, params);
   } catch (error) {
     throw ResponseFactory.createServiceUnavailableResponse(res, error);
